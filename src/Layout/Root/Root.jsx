e7fb378c95a5376367080ee0f7085ed90d3b623d
mdimranictiu <imranictiu11@gmail.com>
2025-01-15 10:37:53 +0600

    navbar added

--- src/Layout/Root/Root.jsx
+++ src/Layout/Root/Root.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import NavBar from '../../Shared/NavBar/NavBar';
 
 const Root = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <h2>This is Root</h2>
             <Outlet></Outlet>
             <h2>This is footer</h2>
---
+++ src/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,180 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { MdDashboardCustomize } from "react-icons/md";
+import { MdLogout } from "react-icons/md";
+import { RiMenu2Line } from "react-icons/ri";
+import { MdClose } from "react-icons/md";
+
+
+
+
+
+const NavBar = () => {
+  const user = true;
+  const dropdownRef=useRef(null)
+  const menuClickRef=useRef(null)
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const [dropdownOpen, setdropdownOpen] = useState(false);
+  const dropdowntoggle = () => {
+    setdropdownOpen(!dropdownOpen);
+  };
+
+  // when user click outside of the target then the item will be close
+  useEffect(()=>{
+    const handleClickOutside=(event)=>{
+        if(dropdownRef.current && !dropdownRef.current.contains(event.target)){
+            setdropdownOpen(false)
+        }
+    }
+    document.addEventListener('mousedown',handleClickOutside);
+    return ()=>{
+        document.removeEventListener('mousedown',handleClickOutside)
+    }
+
+  },[])
+  useEffect(()=>{
+    const handleClickMenuOutside=(event)=>{
+        if(menuClickRef.current && !menuClickRef.current.contains(event.target)){
+            setMenuOpen(false)
+        }
+    }
+    document.addEventListener('mousedown',handleClickMenuOutside);
+    return ()=>{
+        document.removeEventListener('mousedown',handleClickMenuOutside)
+    }
+
+  },[])
+  const links = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/products">Products</Link>
+      </li>
+
+      {
+        // if any user found then this link will be shown
+        user && (
+          <>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+          </>
+        )
+      }
+    </>
+  );
+  const conditionalMenu = (
+    <>
+      {user ? (
+        <div ref={dropdownRef} className="relative flex ml-32 items-center max-sm:items-start gap-6 max-sm:flex-col">
+          <button
+            onClick={dropdowntoggle}
+            className="rounded-full w-12 h-12 bg-gray-500"
+          >
+            <img
+              src={user.photoURL}
+              alt="Profile"
+              className="w-full h-full object-cover"
+            />
+          </button>
+   
+   { dropdownOpen && (
+     <div className="absolute py-3 mt-[230px] max-sm:mt-[60px] -right-[80px] w-48  rounded shadow-lg z-10">
+     <div className="px-4 py-2">
+       <p className="text-gray-700 font-medium">Username</p>
+     </div>
+     <ul className="py-1">
+     <li className="flex items-center px-4 py-2 hover:bg-gray-100">
+              <MdDashboardCustomize className="mr-2" />
+              <Link to="/dashboard" className="text-gray-700">Dashboard</Link>
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100">
+              <MdLogout className="mr-2" />
+              <button className="text-gray-700 w-full text-left">Logout</button>
+            </li>
+     </ul>
+   </div>
+   )}
+         
+        </div>
+      ) : (
+        <div className="flex gap-6 ml-32 text-xl max-sm:gap-12">
+          <button>
+            {" "}
+            <Link to="/login" className="px-4 py-2 text-white bg-black rounded">
+              Login
+            </Link>
+          </button>
+          <button>
+            <Link
+              to="/register"
+              className="px-4 py-2 text-white bg-black rounded"
+            >
+              Register
+            </Link>
+          </button>
+        </div>
+      )}
+    </>
+  );
+
+  return (
+    <div className="bg-white shadow-sm">
+       
+      <div className="flex w-4/5 mx-auto justify-between  px-6  items-center py-5 ">
+      <div className="hidden  px-3 max-md:block">
+        <RiMenu2Line onClick={() => setMenuOpen(!menuOpen)} className="text-2xl"></RiMenu2Line>
+        </div>
+        <h2 className="text-2xl max-sm:text-xl max-sm:ml-24 font-bold">Product Planet</h2>
+        <div className="max-md:hidden ">
+          <ul className="flex ml-32 gap-6 text-xl">{links}</ul>
+        </div>
+        <div>{conditionalMenu}</div>
+        <div></div>
+      </div>
+      <div ref={menuClickRef} className={`fixed top-[90px] left-0 py-10 z-40 w-full bg-gray-500  shadow-lg transform transition-transform duration-300 ${
+          menuOpen ? "translate-x-0" : "-translate-x-full"
+        }`}>
+            <button
+          onClick={() => setMenuOpen(false)}
+          className="absolute top-4 right-4 text-2xl"
+        >
+         <MdClose ></MdClose>
+        </button>
+        <ul className="flex my-5 text-xl flex-col ml-[25px] items-start gap-5">
+        {links}
+        </ul>
+        <div className="flex ml-[25px] flex-col gap-5 items-start">
+          <button>
+            
+            <Link to="/login" className="px-4 py-2 text-white bg-black rounded">
+              Login
+            </Link>
+          </button>
+          <button>
+            <Link
+              to="/register"
+              className="px-4 py-2 text-white bg-black rounded"
+            >
+              Register
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
